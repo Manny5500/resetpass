88aa946b77b5e99c84e12df3c417a623913b9e87
Marvin <marvshaye@gmail.com>
2025-06-21 09:09:11 +0800

    with admin 3

--- src/app/supabaseClient.js
+++ src/app/supabaseClient.js
@@ -1,14 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
       
-/*
+
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
-*/
-export const supabase = createClient(
- "https://tmnsjojcksjeociimree.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRtbnNqb2pja3NqZW9jaWltcmVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAyMDkyMzcsImV4cCI6MjA2NTc4NTIzN30.NXK17YWX4MYibxV92xn2XU8YR-k6KABopp0ZTPUVfiA"
-)
+
+
 
